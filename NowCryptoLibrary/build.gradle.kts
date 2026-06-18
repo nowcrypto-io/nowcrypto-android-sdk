a8c164b4bab92053dc8115447c1d28e848e80043
gemeauxph <khristian.geminiano@gmail.com>
2026-06-18 23:49:57 +0800

    Jitpack pre-requisites

--- NowCryptoLibrary/build.gradle.kts
+++ NowCryptoLibrary/build.gradle.kts
@@ -1,8 +1,13 @@
+import org.gradle.api.publish.PublishingExtension
+import org.gradle.api.publish.maven.MavenPublication
+
 plugins {
     alias(libs.plugins.android.library)
     alias(libs.plugins.kotlin.android)
     alias(libs.plugins.kotlin.compose)
     alias(libs.plugins.kotlin.serialization)
+
+    id("maven-publish")
 }
 
 android {
@@ -20,6 +25,7 @@ android {
     buildTypes {
         release {
             isMinifyEnabled = false
+
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
@@ -31,22 +37,51 @@ android {
         sourceCompatibility = JavaVersion.VERSION_11
         targetCompatibility = JavaVersion.VERSION_11
     }
+
+    // Publish Android library variant
+    publishing {
+        singleVariant("release")
+    }
 }
 
 kotlin {
     compilerOptions {
-        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11)
+        jvmTarget.set(
+            org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_11
+        )
     }
 }
 
+
+// JitPack Maven publication
+afterEvaluate {
+    extensions.configure<PublishingExtension>("publishing") {
+
+        publications {
+            create<MavenPublication>("release") {
+
+                from(components["release"])
+
+                groupId = "com.github.gemeauxph"
+                artifactId = "nowcrypto-sdk"
+                version = "0.0.1"
+            }
+        }
+    }
+}
+
+
 dependencies {
+
     // AndroidX Core & Lifecycle
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
 
+
     // Compose UI
     implementation(libs.androidx.activity.compose)
     implementation(platform(libs.androidx.compose.bom))
+
     implementation(libs.androidx.compose.ui)
     implementation(libs.androidx.compose.ui.graphics)
     implementation(libs.androidx.compose.ui.tooling.preview)
@@ -55,28 +90,36 @@ dependencies {
     implementation(libs.androidx.navigation.compose)
     implementation(libs.androidx.compose.material.icons.extended)
 
+
     // Retrofit & Networking
     implementation(libs.retrofit)
     implementation(libs.okhttp)
     implementation(libs.logging.interceptor)
 
-    // Kotlinx Serialization
+
+    // Kotlin Serialization
     implementation(libs.kotlinx.serialization.json)
     implementation(libs.retrofit.kotlinx.serialization)
+
 
     // Coroutines & DataStore
     implementation(libs.kotlinx.coroutines.android)
     implementation(libs.androidx.datastore.preferences)
 
+
     // Image Loading
     implementation(libs.coil.compose)
 
+
     // Testing
     testImplementation(libs.junit)
+
     androidTestImplementation(libs.androidx.junit)
     androidTestImplementation(libs.androidx.espresso.core)
+
     androidTestImplementation(platform(libs.androidx.compose.bom))
     androidTestImplementation(libs.androidx.compose.ui.test.junit4)
+
     debugImplementation(libs.androidx.compose.ui.tooling)
     debugImplementation(libs.androidx.compose.ui.test.manifest)
 }
