af1a3213ecb49fa8decf1b5eb81d9b9bda42486e
gemeauxph <khristian.geminiano@gmail.com>
2026-09-22 15:36:59 +0800

    Remove tracked folder and add to ignore list

--- NowCryptoLibrary/build.gradle.kts
+++ NowCryptoLibrary/build.gradle.kts
@@ -64,7 +64,7 @@ afterEvaluate {
 
                 groupId = "com.github.nowcrypto-io"
                 artifactId = "nowcrypto-android-sdk"
-                version = "1.0.2"
+                version = "1.0.3"
             }
         }
     }
